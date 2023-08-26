0b9a83753013cdc67afe0a5d268d6a2f492c6de0
MOHAN BABU S R <writeto.mohanbabu@gmail.com>
2023-08-26 21:01:28 +0530

    Update Day-1-Basic.py

--- Day-1/Day-1-Basic.py
+++ Day-1/Day-1-Basic.py
@@ -1,8 +1,33 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2709
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+### Print ###
+# Write your code below this line 👇
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
 
-\f0\fs24 \cf0 Day1}
+# print('Hello ' + input("What is your name? ") + '!')
+print( len( input("What is your name? ") ) )
+
+
+### Variables ###
+name = input("What is your name?")
+print(name)
+
+# challange
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+temp = a
+a = b
+b = temp
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
